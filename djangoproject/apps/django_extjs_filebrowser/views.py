f48fe0001c2f947151039669478b3fec160f7814
Julien Bouquillon <julien@bouquillon.com>
2011-01-05 21:32:24 +0100

    gestion upload v0

--- djangoproject/apps/django_extjs_filebrowser/views.py
+++ djangoproject/apps/django_extjs_filebrowser/views.py
@@ -11,9 +11,10 @@ import datetime
 def dirToJson( inFs, path = '/', recursive = False):
     data = []
     for item in inFs.listdir(path = path ):
-        infos = inFs.getinfo( os.path.join(path, item ) )
-        isLeaf = not inFs.isdir( item )
-        print infos
+        fPath =  os.path.join(path, item ) 
+        infos = inFs.getinfo( fPath )
+        isLeaf = not inFs.isdir( fPath )
+       
         row = {
             'text':item
             ,'size':infos.get('size', 0)
@@ -24,7 +25,7 @@ def dirToJson( inFs, path = '/', recursive = False):
         }
         # recursive and isdir ? 
         if not isLeaf and recursive:
-            row['items'] = dirToJson(inFs, path = os.path.join(path, item), recursive = recursive )
+            row['items'] = dirToJson(inFs, path = fPath, recursive = recursive )
            
         data.append( row )
     return data
@@ -44,6 +45,11 @@ def splitPath( inPath ):
     path = '/'.join(inPath.split('/')[1:])
     return root, path
     
+def getFsFromKey( key ):
+    source = sources[key]
+    cur_fs = source['cls']( **source['params'] )
+    return cur_fs
+    
 @decorators.ajax_request
 def api( request ):
     cmd = request.POST.get('cmd', request.GET.get('cmd'))
@@ -53,7 +59,7 @@ def api( request ):
     # todo: remove these special cases
     if cmd == 'delete':
         root, path = splitPath( request.POST['file'] )
-    if cmd in ['view', 'download']:
+    elif cmd in ['view', 'download']:
         root, path = splitPath( request.GET['file'] )
     elif cmd == 'rename':
         root, path = splitPath( request.POST['oldname'] )
@@ -61,8 +67,7 @@ def api( request ):
         root, path = splitPath( request.POST['path'] )
         
     if root:
-        source = sources[root]
-        cur_fs = source['cls']( **source['params'] )
+        cur_fs = getFsFromKey( root )
     
     if cmd == 'get':
         if not root:
@@ -115,105 +120,53 @@ def download( inFilePath, inFileObj, attachment = False ):
 
     
     
-@decorators.ajax_request    
-def upload( request ):
-    return HttpResponse('api')
-    
-    
-
+ 
 @decorators.swfupload_cookies_auth
 @decorators.ajax_request    
 def upload(request): 
-    path = request.META['X-File-Name']
-    root, path = splitPath( path )
+    path = request.META.get('HTTP_X_FILE_NAME')
+
+    root, path = splitPath( path.decode('UTF-8') )
+    
     file_list = []
     filename_list = []
     
     allowed_extensions = 'jpg,jpeg,gif,png,pdf,swf,avi,mp4,mp3,flv,doc,docx,xls,xlsx,ppt,pptx'.split(',')
     
-    print root, path
-    return {'success':True, 'files':file_list}
-
-    outPath = u'%s' % translateUserPath(request.user, path)
-
-    if not os.path.isdir(outPath):
-        os.makedirs(outPath)
-    if request.GET.get('from') == 'screenshot':
-        outPath = os.path.join( settings.DATA_PATH, 'public', 'user', str(request.user.id), 'screenshots' )
-        if not os.path.isdir(outPath):
-            os.makedirs(outPath)
-        image_64 = request.raw_post_data
-        if image_64:
-            fName ="%s.jpg" % datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-            dstFile = os.path.join(outPath, fName)
-            data = base64.b64decode(image_64)
-            dest = open(dstFile, 'wb')
-            dest.write(data)
-            dest.close()  
-            file_list.append(dstFile)
-            filename_list.append({'file': '/static%s/screenshots/%s' % (get_relative_user_path( request.user.id ), fName)})
-    elif request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-        # htmL5 upload
+    cur_fs = getFsFromKey( root )
+    
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
         data = request.raw_post_data
-
-        fName = request.META['HTTP_X_FILE_NAME'].decode('UTF-8')
-
-        if not os.path.splitext(fName)[1].lower()[1:] in allowed_extensions:
-            # todo : log + mail
-            print 'FORBIDDEN FILE : %s ' % fName.encode('UTF-8')
+        fName = path
+        if not fName[fName.rfind('.')+1:].lower() in allowed_extensions:
+            print 'FORBIDDEN FILE : %s ' % fName
             raise Exception('extension not allowed %s' % fName)
-        dstFile = u'%s' % os.path.join(outPath, fName)
-        utils.CheckPathSecurity(dstFile, outPath)
-        dest = open(dstFile, 'wb')
-        dest.write(data)
-        dest.close()  
-        file_list.append(dstFile)
-        filename_list.append({'file':os.path.split(fName)[1]})
+        f = cur_fs.open( path, 'wb')
+        f.write(data)
+        f.close()
+        # dest.close()  
+        
+        # dstFile = u'%s' % os.path.join(outPath, fName)
+        # utils.CheckPathSecurity(dstFile, outPath)
+        # dest = open(dstFile, 'wb')
+        # dest.write(data)
+        # dest.close()  
+        # file_list.append(dstFile)
+        # filename_list.append({'file':os.path.split(fName)[1]})
+    
     else:
-        # standard multipart upload
         for key in request.FILES.keys():
             upload = request.FILES[key]
             fName = upload.name
             # check extensions
-            if not os.path.splitext(fName)[1].lower()[1:] in allowed_extensions:
+            if not fName[fName.rfind('.')+1:].lower() in allowed_extensions:
                 # todo : log + mail
                 print 'FORBIDDEN FILE : %s ' % fName.encode('UTF-8')
                 raise Exception('extension not allowed %s' % fName.encode('UTF-8'))
-            dstFile = os.path.join(outPath, fName)
-            #print dstFile, outPath
-            utils.CheckPathSecurity( dstFile, outPath)
-            #new_name = utils.guessNextPath(dstFile, slugify = True)
-            dest = open(dstFile, 'wb')
+            f = cur_fs.open( path, 'wb')
             for chunk in upload.chunks():
-                dest.write(chunk)
-            dest.close()         
+                f.write(chunk)
+            f.close()         
             
-            file_list.append(dstFile)            
-            filename_list.append({'file':os.path.split(fName)[1]})
-            
-    for fName in file_list:
-        # PDF 2 jpg conversion
-
-        # gdocs upload
-        if os.path.splitext(fName)[1][1:] in 'xls,xlsx'.split(','):
-            gurl = gapps.upload( fName, os.path.basename( fName ) )
-            # create .gdocs.xls
-            if gurl:
-                linkfile = os.path.join( os.path.dirname( fName ), '%s.gdocs.xls' % os.path.basename( fName ) )
-                open(linkfile, 'w').write('%s&chrome=false' % gurl)
-                #f.close()
-                # delete original
-                os.remove( fName )
-                # wait a while TODO
-                time.sleep(3)
-        else:
-            f = converters.FileObject( fName )
-            res = f.convert()
-            if not res:
-                return extutils.JsonError(_(u'Impossible de convertir ce fichier') )
-                
-    r = extutils.JsonSuccess({"files":filename_list})
-    r['Content-Type'] = "text/html"
-    #import time
-    #time.sleep(5)
-    return r
+    return {'success':True, 'files':file_list}
+ 
